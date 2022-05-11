b2527e266ac2d1beb43179f3663bd3fcbe601d57
Rahulsaraf <63157871+Rahulsaraf1578@users.noreply.github.com>
2022-05-11 13:19:17 +0530

    Made from basic structure
    // Public
    Added fontawesome and google fonts
    
    // App.js
    Importing components and rendering form
    
    // Form.js
    Added form from HTML prewritten

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css"
+import "./components/TodoList"
+import Form from "./components/Form";
+
 function App() {
   return (
     <div className="App">
         <header>
             <h1>Rahul's todo list</h1>
         </header>
+        <Form/>
     </div>
   );
 }
